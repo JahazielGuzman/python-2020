414d932001bc21f51634919a713bb7c68294c80d
JahazielGuzman <jahaziel.guzman93@gmail.com>
2020-01-11 17:10:55 -0500

    changed sockets

--- sockets/server.py
+++ sockets/server.py
@@ -1,5 +1,7 @@
 import socket
 
+HEADERSIZE = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
 s.listen(5)
@@ -7,4 +9,8 @@ s.listen(5)
 while True:
 	clientsocket, address = s.accept()
 	print(f"Connection from {address} has been established!")
+
+	msg = "Welcome to the server!"
+	print(f"{len(msg):<{HEADERSIZE}}"+msg)
+
 	clientsocket.send(bytes("Welcome to the server!", "utf-8"))
